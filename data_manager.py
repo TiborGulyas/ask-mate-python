0487db85da2871e115a9aac7f39ab26a5db8ca0c
nagyakornel <nagyakornel@gmail.com>
2020-01-14 12:53:24 +0100

    Working on SQL requests.

--- data_manager.py
+++ data_manager.py
@@ -16,13 +16,12 @@ def insert_new_question(cursor, title, message, submission_time, view_number, vo
     cursor.execute(f"""
     INSERT INTO question
     (title, message, submission_time, view_number, vote_number, image)
-    VALUES ('{ title }', '{ message }', '{submission_time}', '{view_number}', '{vote_number}', '{image}')
+    VALUES ('{title}', '{message}', '{submission_time}', '{view_number}', '{vote_number}', '{image}')
     """)
     cursor.execute(f"""
     SELECT id FROM question
     WHERE submission_time='{submission_time}';""")
     id = cursor.fetchall()
-    print(id[0]['id'])
     return str(id[0]['id'])
 
 
@@ -30,6 +29,99 @@ def insert_new_question(cursor, title, message, submission_time, view_number, vo
 def get_question_by_id(cursor, id):
     cursor.execute(f"""
     SELECT * FROM question
-    WHERE id={id}""")
+    WHERE id={id};
+    """)
     question = cursor.fetchall()
     return question[0]
+
+
+@connection.connection_handler
+def update_question(cursor, id, title, message, filename, time):
+    cursor.execute(f"""
+    UPDATE question
+    SET title='{title}', message='{message}', image='{filename}', submission_time='{time}'
+    WHERE id={id};
+    """)
+
+
+@connection.connection_handler
+def delete_question(cursor, id):
+    cursor.execute(f"""
+    DELETE FROM question
+    WHERE id={id};
+    """)
+
+
+@connection.connection_handler
+def vote_question(cursor, id, vote):
+    cursor.execute(f"""
+    UPDATE question
+    SET vote_number = vote_number + {vote}
+    WHERE id={id};
+    """)
+
+
+@connection.connection_handler
+def get_answer_by_question_id(cursor, question_id):
+    cursor.execute(f"""
+    SELECT * FROM answer
+    WHERE question_id={question_id};
+    """)
+    answers = cursor.fetchall()
+    print(answers)
+    return answers
+
+
+@connection.connection_handler
+def insert_new_answer(cursor, message, submission_time, vote_number, image, question_id):
+    cursor.execute(f"""
+    INSERT INTO answer
+    (message, submission_time, vote_number, image, question_id)
+    VALUES ('{message}', '{submission_time}', '{vote_number}', '{image}', '{question_id}')
+    """)
+
+
+@connection.connection_handler
+def get_answer_by_id(cursor, id):
+    cursor.execute(f"""
+    SELECT * FROM answer
+    WHERE id={id};
+    """)
+    answer = cursor.fetchall()
+    return answer
+
+
+@connection.connection_handler
+def delete_answer(cursor, id):
+    cursor.execute(f"""
+    SELECT question_id FROM answer
+    WHERE id={id};""")
+    question_id = cursor.fetchall()
+    cursor.execute(f"""
+    DELETE FROM answer
+    WHERE id={id};
+    """)
+    return str(question_id[0]['question_id'])
+
+
+@connection.connection_handler
+def vote_answer(cursor, id, vote):
+    cursor.execute(f"""
+    UPDATE answer
+    SET vote_number = vote_number + {vote}
+    WHERE id={id};
+    """)
+    cursor.execute(f"""
+    SELECT question_id FROM answer
+    WHERE id={id};""")
+    question_id = cursor.fetchall()
+    return str(question_id[0]['question_id'])
+
+
+@connection.connection_handler
+def insert_image(cursor, id, table, image):
+    cursor.execute(f"""
+    UPDATE {table}
+    SET image = '{image}'
+    WHERE id={id};
+    """)
--- server.py
+++ server.py
@@ -7,7 +7,7 @@ app = Flask(__name__, static_url_path='/static')
 
 DATA_HEADER_question = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
 DATA_HEADER_answer = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
-UPLOAD_folder = './uploaded_image/'
+UPLOAD_folder = './'
 app.config['UPLOAD_FOLDER'] = UPLOAD_folder
 
 
@@ -38,17 +38,23 @@ def add_question():
         new_question = {'title': request.form.get('title'), 'message': request.form.get('message'),
                         'submission_time': util.generate_time(), 'view_number': '0', 'vote_number': '0',
                         'image': 'not found'}
+        new_question['id'] = data_manager.insert_new_question(*new_question.values())
         try:
             file = request.files['file']
             if file and util.allowed_file(file.filename):
                 filename_original = file.filename.split('.')
+                print('filename original megvan')
                 filename = ".".join([new_question['id'], filename_original[-1]])
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                print('filename megvan')
+                print(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                file.save(filename)
+                print('file mentés megvan')
                 new_question['image'] = "uploaded-image/" + filename
+                print('sql kapcsolat megvan')
         except FileNotFoundError:
+            print('nem találom')
             pass
-        new_question['id'] = data_manager.insert_new_question(*new_question.values())
-
+        data_manager.insert_image(new_question['id'], 'question', new_question['image'])
         return redirect("/question/" + new_question['id'])
 
 
@@ -56,67 +62,56 @@ def add_question():
 def view_question(question_id):
     if request.method == 'GET' and question_id.isdigit():
         question_for_display = data_manager.get_question_by_id(question_id)
+        answer_for_display = data_manager.get_answer_by_question_id(question_id)
+        if len(answer_for_display) == 0:
+            answer_for_display = [{'message': 'No answer yet', 'submission_time': '', 'vote_number': '', 'image': ''}]
         return render_template(
             'question.html',
-            question_for_display=question_for_display)
+            question_for_display=question_for_display, answer_for_display=answer_for_display)
 
 
 @app.route('/question/<question_id>/edit', methods=['GET', 'POST'])
 def edit_question(question_id):
     if request.method == 'GET':
-        return render_template('add_new_question.html', output_dict=util.get_data_of_editable_question(question_id))
+        return render_template('new-question.html', output_dict=data_manager.get_question_by_id(question_id))
     elif request.method == 'POST':
+        filename = 'not found'
         try:
             filename = util.generate_file_name_for_image(request.files['file'], question_id)
             request.files['file'].save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         except TypeError:
-            filename = ""
-        util.update_question(question_id, request.form.get('message'), request.form.get('title'), filename)
+            filename = 'TypeError'
+        data_manager.update_question(question_id, request.form.get('message'), request.form.get('title'), filename, util.generate_time())
         return redirect('/')
 
 
 @app.route('/question/<question_id>/delete', methods=['GET', 'POST'])
 def delete_question(question_id):
-    question_dictionary_list = data_manager.get_data('question')
-    for number, dict_in_data in enumerate(question_dictionary_list):
-        if dict_in_data['id'] == int(question_id):
-            try:
-                question_dictionary_list[number]['image'] = \
-                    list(question_dictionary_list[number]['image'].split('/'))[1]
-                os.remove(os.path.join(app.config['UPLOAD_FOLDER'], question_dictionary_list[number]['image']))
-            except IsADirectoryError:
-                pass
-            del question_dictionary_list[number]
-    data_manager.write_data('question', question_dictionary_list)
+    data_manager.delete_question(question_id)
     return redirect('/')
 
 
 @app.route('/question/<question_id>/<vote>', methods=['GET', 'POST'])
 def question_vote(question_id, vote):
-    question_dictionary_list = data_manager.get_data('question')
-    for number, dict_in_data in enumerate(question_dictionary_list):
-        if dict_in_data['id'] == int(question_id):
-            if vote == "vote_up":
-                question_dictionary_list[number]['vote_number'] += 1
-            else:
-                question_dictionary_list[number]['vote_number'] -= 1
-    data_manager.write_data('question', question_dictionary_list)
+    if vote == "vote_up":
+        data_manager.vote_question(question_id, 1)
+    else:
+        data_manager.vote_question(question_id, -1)
     return redirect('/')
 
 
 @app.route('/question/<question_id>/new-answer', methods=['GET', 'POST'])
 def add_answer(question_id):
-    question_for_display = util.get_data_of_editable_question(question_id)
+    question_for_display = data_manager.get_question_by_id(question_id)
     if request.method == 'GET':
         return render_template(
             'new-answer.html',
             question_for_display=question_for_display,
-            header=DATA_HEADER_question,
             question_id=question_id)
     elif request.method == "POST":
-        new_answer = {'message': request.form.get('message'), 'id': util.generate_id('answer'),
+        new_answer = {'message': request.form.get('message'),
                       'submission_time': util.generate_time(), 'vote_number': '0', 'image': '',
-                      'question_id': int(question_id)}
+                      'question_id': int(question_id), 'image': 'not found'}
         try:
             file = request.files['file']
             if file and util.allowed_file(file.filename):
@@ -126,46 +121,29 @@ def add_answer(question_id):
                 new_answer['image'] = "uploaded-image/" + filename
         except FileNotFoundError:
             new_answer['image'] = ''
-        answer_dictionary_list = data_manager.get_data('answer')
-        answer_dictionary_list.append(new_answer)
-        data_manager.write_data('answer', answer_dictionary_list)
-        answer_dictionary_list = data_manager.get_data('answer')
-        answer_for_display = []
-        for answer in answer_dictionary_list:
-            if int(answer['question_id']) == int(question_id):
-                answer_for_display.append(answer)
+
+        data_manager.insert_new_answer(*new_answer.values())
         return redirect(f'/question/{question_id}')
 
 
 @app.route('/answer/<answer_id>', methods=['GET', 'POST'])
 def show_answer(answer_id):
-    for answer in data_manager.get_data('answer'):
-        if answer['id'] == int(answer_id):
-            answer_for_display = answer
-    return render_template('answer.html', answer_for_display=answer_for_display, answer_header=DATA_HEADER_answer)
+    answer_for_display = data_manager.get_answer_by_id(answer_id)
+    return render_template('answer.html', answer_for_display=answer_for_display[0])
 
 
 @app.route('/answer/<answer_id>/delete', methods=['GET', 'POST'])
 def delete_answer(answer_id):
-    answer_dictionary_list = data_manager.get_data('answer')
-    for number, dict in enumerate(answer_dictionary_list):
-        if dict['id'] == int(answer_id):
-            del answer_dictionary_list[number]
-    data_manager.write_data('answer', answer_dictionary_list)
-    return redirect('/')
+    question_id = data_manager.delete_answer(answer_id)
+    return redirect(f'/question/{question_id}')
 
 
 @app.route('/answer/<answer_id>/<vote>', methods=['GET', 'POST'])
 def answer_vote(answer_id, vote):
-    answer_dictionary_list = data_manager.get_data('answer')
-    for number, dict_in_data in enumerate(answer_dictionary_list):
-        if dict_in_data['id'] == int(answer_id):
-            if vote == "vote_up":
-                answer_dictionary_list[number]['vote_number'] += 1
-            else:
-                answer_dictionary_list[number]['vote_number'] -= 1
-            question_id = dict_in_data['question_id']
-    data_manager.write_data('answer', answer_dictionary_list)
+    if vote == "vote_up":
+        question_id = data_manager.vote_answer(answer_id, 1)
+    else:
+        question_id = data_manager.vote_answer(answer_id, -1)
     return redirect(f'/question/{question_id}')
 
 
